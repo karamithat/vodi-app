db98d53652b4724a445ae81fafb613c9c5fb3e61
karamithat <mithatkara93@gmail.com>
2023-04-09 23:23:24 +0300

    Movie detail page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import "./style.scss"
 
+import useFetch from '../../hooks/useFetch'
+import DetailsBanner from "../details/detailsBanner/DetailsBanner"
+
 const Details = () => {
+  // const { mediaType, id } = useParams()
+
+  // const {data,loading} = useFetch(`/${mediaType}/${id}`)
+  
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
 
